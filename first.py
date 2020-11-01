5052623871c2cbadaac10ccdf4ff9bc9be008643
lkkplau0988 <lkkplau0988@gmail.com>
2020-11-01 15:49:20 +0800

    modify first.py

--- first.py
+++ first.py
@@ -1,1 +1,11 @@
 print("hi! let's begin!")
+import scrapy
+from bs4 import BeautifulSoup
+class AppleCrawler(scrapy.Spider):
+    name='apple'
+    start_urls=['https://hk.appledaily.com']
+    def parse(self,response):
+        res=BeautifulSoup(response.body)
+        for news in res.select('html'):
+            print(news.select('lang')[0].text)
+
