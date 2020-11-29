3ebf7d4d134995e47c01b66bafc2e0b7e09c55d1
lkkplau0988 <lkkplau0988@gmail.com>
2020-11-29 11:18:54 +0800

    add file Account.py

---
+++ Account.py
@@ -0,0 +1,10 @@
+class Account:
+
+    def __init__(self):
+        self.balance=balance
+
+    def getMonthlyInterestRate(self):
+        return (annualInterest/100)
+
+    def getMonthlyInterest(self):
+        return blanace * monthlyInterestRate
