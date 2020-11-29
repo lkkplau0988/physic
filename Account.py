d0fe70ac2f4db3d0eb3eeb9c51f39c2328357b06
lkkplau0988 <lkkplau0988@gmail.com>
2020-11-29 21:42:40 +0800

    amend account.py

--- Account.py
+++ Account.py
@@ -1,14 +1,25 @@
 class Account:
-
-    def __init__(self):
+    
+    def __init__(self,balance):
         self.balance=balance
 
-    def getMonthlyInterestRate(self):
+    def getMonthlyInterestRate(self,annualInterest):
+        self.annualInterest=annualInterest
         return (annualInterest/100)
-
     def getMonthlyInterest(self):
-        return blanace * monthlyInterestRate
-kei=Account(1000)
-print(kei.balance())
-kei.getMonthlyInterestRate(5)
+        return kei.balance * monthlyInterestRate	        
+
+    def display(self):
+        print(self.balance,kei.getMonthlyInterestRate(2))
+        
+kei=Account(200)
+kei.display()
+print(kei.balance)
+monthlyInterestRate=kei.getMonthlyInterestRate(6)
+print(monthlyInterestRate)
 print(kei.getMonthlyInterest())
+
+
+
+
+
