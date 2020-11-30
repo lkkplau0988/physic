89ab52a6c1410345a82690f06f4865cb3a945427
lkkplau0988 <lkkplau0988@gmail.com>
2020-11-30 22:22:58 +0800

    add file class begin2

--- class_study_begin1.py
+++ class_study_begin1.py
@@ -1,12 +1,10 @@
 class Person:
     def setName(self, name):
-        self.name = name.title()
+        self.name = name
     def getName(self):
         return self.name
-    
 # end of class definition
 #start of program
 person1 = Person() #create instance of class
-person1.setName('Mr. Smith') #set name
-print(person1.getName()) # get name
-
+person1.setName('John') 
+print(person1.getName())
